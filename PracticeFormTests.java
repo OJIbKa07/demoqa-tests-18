8290b73df7c099daeac5667ea70596f157e65036
Olga Palushina <lokipypzemli@mail.ru>
2025-06-12 00:06:59 +0300

    added TextBoxTests changed build.gradle

---
+++ PracticeFormTests.java
@@ -0,0 +1,4 @@
+public class PracticeFormTests {
+
+    @Test
+}
--- src/test/java/TextBoxTests.java
+++ src/test/java/TextBoxTests.java
@@ -1,14 +1,24 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
+
 public class TextBoxTests {
+
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
 
     @Test
     void fullFormTest() {
-        open("https://demoqa.com/text-box");
+        open("/text-box");
 
         $("#userName").setValue("Olga Palushina");
         $("#userEmail").setValue("lqoka2015@yandex.ru");
@@ -17,5 +27,6 @@ public class TextBoxTests {
 
         $("#submit").click();
         $("#output").shouldHave(text("Olga Palushina"), text("lqoka2015@yandex.ru"), text("Russia, Kaluga"), text("Russia, Kaluga"));
+
     }
 }
